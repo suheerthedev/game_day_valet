d7f476494ce9322363a53dae6a883fa45ef2ae17
suheerthedev <khansuheer1@gmail.com>
2025-07-24 14:21:51 +0500

    Completed RentalActive Status

--- lib/ui/views/rental_status/rental_status_view.dart
+++ lib/ui/views/rental_status/rental_status_view.dart
@@ -1,8 +1,11 @@
+import 'package:dotted_line/dotted_line.dart';
 import 'package:floating_chat_button/floating_chat_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:game_day_valet/ui/common/app_colors.dart';
 import 'package:game_day_valet/ui/widgets/common/main_app_bar/main_app_bar.dart';
+import 'package:game_day_valet/ui/widgets/common/main_button/main_button.dart';
+import 'package:game_day_valet/ui/widgets/common/main_text_field/main_text_field.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:stacked/stacked.dart';
@@ -34,7 +37,9 @@ class RentalStatusView extends StackedView<RentalStatusViewModel> {
                 padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 20.h),
                 child: Stack(
                   children: [
-                    _buildNoRentalState(context),
+                    viewModel.isRentalActive
+                        ? _buildRentalActiveState(context, viewModel)
+                        : _buildNoRentalState(context),
                     FloatingChatButton(
                       onTap: (_) {
                         viewModel.onChatTap();
@@ -94,9 +99,269 @@ class RentalStatusView extends StackedView<RentalStatusViewModel> {
     );
   }
 
+  Widget _buildRentalActiveState(
+      BuildContext context, RentalStatusViewModel viewModel) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "00",
+                      style: GoogleFonts.poppins(
+                          fontSize: 40.sp,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.textHint),
+                    ),
+                    Text(
+                      'hours',
+                      style: GoogleFonts.poppins(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w400,
+                          color: AppColors.textHint),
+                    ),
+                  ],
+                ),
+                Text(
+                  " : ",
+                  style: GoogleFonts.poppins(
+                      fontSize: 40.sp,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.textHint),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${viewModel.timeRemaining}',
+                      style: GoogleFonts.poppins(
+                          fontSize: 40.sp,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.textSecondary),
+                    ),
+                    Text(
+                      'minutes',
+                      style: GoogleFonts.poppins(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w400,
+                          color: AppColors.textHint),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 20.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "time left for delivery",
+                  style: GoogleFonts.poppins(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.textHint),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 30.h),
+            // Order tracking timeline
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: viewModel.trackingSteps.length,
+              itemBuilder: (context, index) {
+                final step = viewModel.trackingSteps[index];
+                final isLastItem = index == viewModel.trackingSteps.length - 1;
+
+                return _buildTimelineItem(
+                  title: step.title,
+                  timestamp: step.timestamp,
+                  isCompleted: step.isCompleted,
+                  isSecondLastItem: index == viewModel.trackingSteps.length - 2,
+                  isLastItem: isLastItem,
+                );
+              },
+            ),
+            SizedBox(height: 20.h),
+            Text(
+              'Return Instructions',
+              style: GoogleFonts.poppins(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.primary),
+            ),
+            SizedBox(height: 10.h),
+            MainTextField(
+                label: 'Special Instructions',
+                controller: viewModel.specialInstructionsController),
+            SizedBox(height: 10.h),
+            Text(
+              'Delivery Photos',
+              style: GoogleFonts.poppins(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.primary),
+            ),
+
+            SizedBox(height: 10.h),
+            SizedBox(
+              width: double.infinity,
+              height: 106.h,
+              child: GridView.builder(
+                scrollDirection: Axis.horizontal,
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 123.w,
+                    childAspectRatio: 123.w / 106.h,
+                    crossAxisSpacing: 10.w,
+                    mainAxisSpacing: 10.h,
+                    mainAxisExtent: 106.h),
+                itemCount: 2,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 123.w,
+                    height: 106.h,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage('assets/images/delivery_photo_2.png')),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 10.h),
+            Text(
+              'Give Review',
+              style: GoogleFonts.poppins(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.primary),
+            ),
+            SizedBox(height: 10.h),
+            MainTextField(
+                label: 'Write Google Review Here',
+                controller: viewModel.googleReviewController),
+
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                'Need Help?',
+                style: GoogleFonts.poppins(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.textSecondary),
+              ),
+            ),
+            SizedBox(height: 20.h),
+            MainButton(
+              onTap: () {},
+              text: 'Order More',
+              color: AppColors.secondary,
+              borderColor: AppColors.secondary,
+              textColor: AppColors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTimelineItem({
+    required String title,
+    required String timestamp,
+    required bool isCompleted,
+    required bool isSecondLastItem,
+    required bool isLastItem,
+  }) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Timeline indicator
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Timeline dot
+            Container(
+              width: 11.w,
+              height: 11.h,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isCompleted ? AppColors.secondary : AppColors.grey300,
+                // border: Border.all(
+                //   color: isCompleted ? AppColors.secondary : AppColors.grey300,
+                //   width: 3.w,
+                // ),
+              ),
+            ),
+            // Timeline line
+            if (!isLastItem)
+              DottedLine(
+                direction: Axis.vertical,
+                dashColor:
+                    isSecondLastItem ? AppColors.grey300 : AppColors.secondary,
+                lineLength: 70.h,
+                dashLength: 4,
+                dashGapLength: 2,
+                dashGapColor: Colors.transparent,
+              ),
+          ],
+        ),
+        SizedBox(width: 12.w),
+        // Content
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: GoogleFonts.poppins(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w600,
+                letterSpacing: -0.7.sp,
+                color: isCompleted ? AppColors.textPrimary : AppColors.textHint,
+              ),
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.access_time_rounded,
+                  size: 12.sp,
+                  color: AppColors.textHint,
+                ),
+                SizedBox(width: 4.w),
+                Text(
+                  timestamp,
+                  style: GoogleFonts.poppins(
+                    fontSize: 15.sp,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: -0.7.sp,
+                    color: AppColors.textHint,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   RentalStatusViewModel viewModelBuilder(
     BuildContext context,
   ) =>
       RentalStatusViewModel();
+
+  @override
+  void onViewModelReady(RentalStatusViewModel viewModel) {
+    viewModel.startPeriodicTimer();
+    super.onViewModelReady(viewModel);
+  }
 }
--- lib/ui/views/rental_status/rental_status_viewmodel.dart
+++ lib/ui/views/rental_status/rental_status_viewmodel.dart
@@ -1,12 +1,83 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:game_day_valet/app/app.locator.dart';
 import 'package:game_day_valet/app/app.router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
+class OrderTrackingStep {
+  final String title;
+  final String timestamp;
+  final bool isCompleted;
+
+  OrderTrackingStep({
+    required this.title,
+    required this.timestamp,
+    required this.isCompleted,
+  });
+}
+
 class RentalStatusViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
 
+  TextEditingController specialInstructionsController = TextEditingController();
+  TextEditingController googleReviewController = TextEditingController();
+
+  bool get isRentalActive => true;
+
+  Timer? timer; // Declare a Timer variable
+  int timeRemaining = 25; // Example: 60 seconds
+  bool isRunning = false;
+
   void onChatTap() {
     _navigationService.navigateToChatView();
+  }
+
+  void startPeriodicTimer() {
+    isRunning = true;
+    rebuildUi();
+    timer = Timer.periodic(const Duration(minutes: 1), (timer) {
+      if (timeRemaining > 0) {
+        timeRemaining--;
+        print('Time remaining: $timeRemaining');
+        rebuildUi();
+        // Update UI (e.g., using setState)
+      } else {
+        timer.cancel();
+        print('Countdown finished!');
+        this.timer = null;
+        isRunning = false;
+        rebuildUi();
+      }
+    });
+  }
+
+  final List<OrderTrackingStep> _trackingSteps = [
+    OrderTrackingStep(
+      title: 'Rental Confirm',
+      timestamp: '05:58 PM, 31 Jan 2025',
+      isCompleted: true,
+    ),
+    OrderTrackingStep(
+      title: 'Out for Delivery',
+      timestamp: '05:58 PM, 31 Jan 2025',
+      isCompleted: true,
+    ),
+    OrderTrackingStep(
+      title: 'Delivery',
+      timestamp: '05:58 PM, 31 Jan 2025',
+      isCompleted: false,
+    ),
+  ];
+
+  List<OrderTrackingStep> get trackingSteps => _trackingSteps;
+
+  String getOrderId() {
+    return '#1234567890';
+  }
+
+  String getEstimatedDeliveryTime() {
+    return '30 minutes';
   }
 }
