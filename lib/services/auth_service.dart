8a0c90fdd82a30d4ee31062ab3e61056e2146d76
suheerthedev <khansuheer1@gmail.com>
2025-07-29 14:33:34 +0500

    Updated AuthService, with register and login method

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,1 +1,52 @@
-class AuthService {}
+import 'package:game_day_valet/app/app.locator.dart';
+import 'package:game_day_valet/config/api_config.dart';
+import 'package:game_day_valet/services/api_exception.dart';
+import 'package:game_day_valet/services/api_service.dart';
+import 'package:game_day_valet/services/secure_storage_service.dart';
+// import 'package:game_day_valet/services/user_service.dart';
+
+class AuthService {
+  final _apiService = locator<ApiService>();
+  final _secureStorageService = locator<SecureStorageService>();
+  // final _userService = locator<UserService>();
+
+  Future<void> login({required String email, required String password}) async {
+    final url = ApiConfig.baseUrl + ApiConfig.loginEndPoint;
+
+    try {
+      final response =
+          await _apiService.post(url, {'email': email, 'password': password});
+
+      if (!response.containsKey('token')) {
+        throw ApiException("Token not found in response", 500);
+      }
+
+      final tokenParts = response['token'].split('|');
+      if (tokenParts.length != 2) {
+        throw ApiException("Invalid token format", 500);
+      }
+
+      final token = tokenParts[1];
+      await _secureStorageService.saveToken(token);
+
+      // final user = UserModel.fromJson(response['user']);
+      // await _userService.getUser(user);
+    } on ApiException catch (_) {
+      rethrow;
+    } catch (_) {
+      throw ApiException("Login Failed.");
+    }
+  }
+
+  Future<void> register(Map<String, dynamic> body) async {
+    final url = ApiConfig.baseUrl + ApiConfig.registerEndPoint;
+
+    try {
+      await _apiService.post(url, body);
+    } on ApiException catch (_) {
+      rethrow;
+    } catch (_) {
+      throw ApiException("Registration Failed.");
+    }
+  }
+}
