6dca7a9dec31571fa6c2232447c8f820e5527a5e
suheerthedev <khansuheer1@gmail.com>
2025-08-01 18:45:10 +0500

    Fixed SDK Version errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.gdv.game_day_valet"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -43,3 +43,12 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    // Firebase BoM (Bill of Materials)
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+
+    // Firebase Auth (for Google Sign-In)
+    implementation("com.google.firebase:firebase-auth")
+}
+
