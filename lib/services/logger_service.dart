50a7e582066f379f6d46a1bef8ac8db31ca5baed
suheerthedev <khansuheer1@gmail.com>
2025-08-01 15:18:13 +0500

    Updated LoggerService

--- lib/services/logger_service.dart
+++ lib/services/logger_service.dart
@@ -1,1 +1,47 @@
-class LoggerService {}
+import 'package:logger/logger.dart';
+
+class LoggerService {
+  static final LoggerService _instance = LoggerService._internal();
+  factory LoggerService() => _instance;
+  LoggerService._internal();
+
+  late final Logger _logger;
+
+  void intialize({bool isProduction = false}) {
+    _logger = Logger(
+      filter: isProduction ? ProductionFilter() : DevelopmentFilter(),
+      printer: PrettyPrinter(
+        methodCount: 2,
+        errorMethodCount: 8,
+        lineLength: 120,
+        colors: true,
+        printEmojis: true,
+        printTime: true,
+      ),
+      output: ConsoleOutput(),
+    );
+  }
+
+  //Convenience methods
+  void debug(String message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.d(message, error, stackTrace);
+  }
+
+  void info(String message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.i(message, error, stackTrace);
+  }
+
+  void warning(String message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.w(message, error, stackTrace);
+  }
+
+  void error(String message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.e(message, error, stackTrace);
+  }
+
+  void wtf(String message, [dynamic error, StackTrace? stackTrace]) {
+    _logger.w(message, error, stackTrace);
+  }
+}
+
+final logger = LoggerService();
