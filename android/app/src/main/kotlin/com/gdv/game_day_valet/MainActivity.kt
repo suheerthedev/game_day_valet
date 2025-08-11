7764e0db9e7558c4a97bc1df4d4161b15d90e803
suheerthedev <khansuheer1@gmail.com>
2025-08-11 20:42:56 +0500

    Updated MainActivity.kt

--- android/app/src/main/kotlin/com/gdv/game_day_valet/MainActivity.kt
+++ android/app/src/main/kotlin/com/gdv/game_day_valet/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.gdv.game_day_valet
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
