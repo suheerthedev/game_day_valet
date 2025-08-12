88d478c8c0b9e5482e8d44743360133825ff9265
suheerthedev <khansuheer1@gmail.com>
2025-08-12 19:52:01 +0500

    Created MainItemCard

---
+++ lib/ui/widgets/common/main_item_card/main_item_card.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'main_item_card_model.dart';
+
+class MainItemCard extends StackedView<MainItemCardModel> {
+  const MainItemCard({super.key});
+
+  @override
+  Widget builder(
+    BuildContext context,
+    MainItemCardModel viewModel,
+    Widget? child,
+  ) {
+    return const SizedBox.shrink();
+  }
+
+  @override
+  MainItemCardModel viewModelBuilder(
+    BuildContext context,
+  ) => MainItemCardModel();
+}
