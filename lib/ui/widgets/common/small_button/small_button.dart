76d5297f267fe95519fefb6d69fd4095ccfa7d30
suheerthedev <khansuheer1@gmail.com>
2025-08-12 20:05:07 +0500

    Updated SmallButton

--- lib/ui/widgets/common/small_button/small_button.dart
+++ lib/ui/widgets/common/small_button/small_button.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
 
 import 'small_button_model.dart';
 
 class SmallButton extends StackedView<SmallButtonModel> {
-  const SmallButton({super.key});
+  final String title;
+  final VoidCallback onTap;
+  final double? height;
+  final double? width;
+  final Color bgColor;
+  final Color textColor;
+  final Color borderColor;
+  final bool isLoading;
+  const SmallButton({
+    super.key,
+    required this.title,
+    required this.onTap,
+    this.height,
+    this.width,
+    required this.bgColor,
+    required this.textColor,
+    required this.borderColor,
+    this.isLoading = false,
+  });
 
   @override
   Widget builder(
@@ -12,7 +32,41 @@ class SmallButton extends StackedView<SmallButtonModel> {
     SmallButtonModel viewModel,
     Widget? child,
   ) {
-    return const SizedBox.shrink();
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+          width: 170.w,
+          height: 40.h,
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: BorderRadius.circular(10.r),
+            border: Border.all(
+              color: borderColor,
+              width: 1,
+            ),
+          ),
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                isLoading
+                    ? SizedBox(
+                        width: 20.w,
+                        height: 20.h,
+                        child: const CircularProgressIndicator(),
+                      )
+                    : Text(
+                        title,
+                        style: GoogleFonts.inter(
+                          color: textColor,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16.sp,
+                        ),
+                      ),
+              ],
+            ),
+          )),
+    );
   }
 
   @override
