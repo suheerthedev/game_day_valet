0095f6112eaf6ba7b9a802f46e684c6ae5c3bdf0
suheerthedev <khansuheer1@gmail.com>
2025-08-13 14:56:35 +0500

    Created MessageModel

---
+++ lib/models/message_model.dart
@@ -0,0 +1,36 @@
+import 'package:game_day_valet/models/user_model.dart';
+
+class MessageModel {
+  final int id;
+  final int conversationId;
+  final int senderId;
+  final String content;
+  final int isRead;
+  final String? createdAt;
+  final String? updatedAt;
+  final UserModel? sender;
+
+  MessageModel({
+    required this.id,
+    required this.conversationId,
+    required this.senderId,
+    required this.content,
+    required this.isRead,
+    this.createdAt,
+    this.updatedAt,
+    this.sender,
+  });
+
+  factory MessageModel.fromJson(Map<String, dynamic> json) {
+    return MessageModel(
+      id: json['id'],
+      conversationId: json['conversation_id'],
+      senderId: json['sender_id'],
+      content: json['content'],
+      isRead: json['is_read'],
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
+      sender: UserModel.fromJson(json['sender']),
+    );
+  }
+}
