6392b1bbe964be169d7b3dc8367ffcf6809735d0
suheerthedev <khansuheer1@gmail.com>
2025-08-13 14:38:57 +0500

    Updated Chat to Inbox

--- lib/ui/views/chat/chat_view.dart
+++ lib/ui/views/chat/chat_view.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:game_day_valet/ui/common/app_colors.dart';
-import 'package:game_day_valet/ui/widgets/common/main_app_bar/main_app_bar.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:iconsax_plus/iconsax_plus.dart';
 import 'package:stacked/stacked.dart';
 
 import 'chat_viewmodel.dart';
@@ -19,58 +15,9 @@ class ChatView extends StackedView<ChatViewModel> {
   ) {
     return Scaffold(
       backgroundColor: AppColors.scaffoldBackground,
-      appBar: MainAppBar(
-        title: Text(
-          'Chat Inbox',
-          style: GoogleFonts.poppins(
-              fontSize: 24.sp,
-              fontWeight: FontWeight.w600,
-              color: AppColors.textPrimary),
-        ),
-      ),
-      body: SafeArea(
-        child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25.w),
-            child: ListView.separated(
-              itemCount: 5,
-              separatorBuilder: (context, index) => Divider(
-                height: 1.h,
-                color: AppColors.grey200,
-              ),
-              itemBuilder: (context, index) {
-                return ListTile(
-                  contentPadding: EdgeInsets.zero,
-                  onTap: () {},
-                  leading: CircleAvatar(
-                    radius: 22.r,
-                    backgroundColor: AppColors.secondary,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(22.r),
-                      clipBehavior: Clip.hardEdge,
-                      child: Image.asset('assets/images/pfp.png'),
-                    ),
-                  ),
-                  title: Text("Katie",
-                      style: GoogleFonts.poppins(
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w600,
-                          color: AppColors.textPrimary)),
-                  subtitle: Text("Hello, how are you?",
-                      style: GoogleFonts.poppins(
-                          fontSize: 12.sp,
-                          fontWeight: FontWeight.w400,
-                          color: AppColors.textHint)),
-                );
-              },
-            )),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(IconsaxPlusLinear.add, color: AppColors.white),
-        backgroundColor: AppColors.secondary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.r),
-        ),
+      body: Container(
+        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+        child: const Center(child: Text("ChatView")),
       ),
     );
   }
--- lib/ui/views/inbox/inbox_view.dart
+++ lib/ui/views/inbox/inbox_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:game_day_valet/ui/common/app_colors.dart';
+import 'package:game_day_valet/ui/widgets/common/main_app_bar/main_app_bar.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax_plus/iconsax_plus.dart';
 import 'package:stacked/stacked.dart';
 
 import 'inbox_viewmodel.dart';
@@ -13,10 +18,59 @@ class InboxView extends StackedView<InboxViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-        child: const Center(child: Text("InboxView")),
+      backgroundColor: AppColors.scaffoldBackground,
+      appBar: MainAppBar(
+        title: Text(
+          'Chat Inbox',
+          style: GoogleFonts.poppins(
+              fontSize: 24.sp,
+              fontWeight: FontWeight.w600,
+              color: AppColors.textPrimary),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.w),
+            child: ListView.separated(
+              itemCount: 5,
+              separatorBuilder: (context, index) => Divider(
+                height: 1.h,
+                color: AppColors.grey200,
+              ),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  onTap: () {},
+                  leading: CircleAvatar(
+                    radius: 22.r,
+                    backgroundColor: AppColors.secondary,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(22.r),
+                      clipBehavior: Clip.hardEdge,
+                      child: Image.asset('assets/images/pfp.png'),
+                    ),
+                  ),
+                  title: Text("Katie",
+                      style: GoogleFonts.poppins(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.textPrimary)),
+                  subtitle: Text("Hello, how are you?",
+                      style: GoogleFonts.poppins(
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w400,
+                          color: AppColors.textHint)),
+                );
+              },
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: AppColors.secondary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.r),
+        ),
+        child: const Icon(IconsaxPlusLinear.add, color: AppColors.white),
       ),
     );
   }
